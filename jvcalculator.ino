9cbb5331f796b09dd71075b5e532325c5c2d7a51
Joan Vilarrasa <joan.vilarrasa96@gmail.com>
2025-12-30 15:58:44 +0100

    small fix

--- jvcalculator.ino
+++ jvcalculator.ino
@@ -143,13 +143,17 @@ void loop() {
   char queryToPrint[LCD_LINE_LENGTH] = "";
   int emptySpaces = LCD_LINE_LENGTH - cursorIndex;
   for(int i = 0; i < emptySpaces; i++) {
-    queryToPrint[cursorIndex + i] = ' ';
+    queryToPrint[cursorIndex + i] = '';
   }
   if(emptySpaces < 0) {
-    emptySpaces = 0;
+    for(int i = 0; i < LCD_LINE_LENGTH; i++) {
+      queryToPrint[i] = query[i - emptySpaces];
+    }
   }
-  for(int i = emptySpaces; i < LCD_LINE_LENGTH; i++) {
-    queryToPrint[i] = query[i - emptySpaces];
+  else {
+    for(int i = emptySpaces; i < LCD_LINE_LENGTH; i++) {
+      queryToPrint[i] = query[i - emptySpaces];
+    }
   }
   lcd.setCursor(0,0);
   lcd.print(queryToPrint);
